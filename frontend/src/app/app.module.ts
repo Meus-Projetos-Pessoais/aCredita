ff042a5a267ea806a8d5a74de957c13d5927e4a5
Andre Emidio <andresjc2008@gmail.com>
2019-09-11 08:53:36 -0300

    Atualização

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +23,12 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'delete/:id', component: DeleteComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full'}
+];
+
 export class AppModule { }
